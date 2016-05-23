cfb2634a0b37cd5b3f0644b3ca6b8fa7a1ac9d75
Will Maier <wcmaier@gmail.com>
2016-05-23 06:49:36 +0000

    Quote SSH_AUTH_SOCK

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ case "$1" in
         rm -f "${SSH_AUTH_SOCK}"
         mkdir -p $(dirname "${SSH_AUTH_SOCK}")
         chmod 700 $(dirname "${SSH_AUTH_SOCK}")
-        exec /usr/bin/ssh-agent -a ${SSH_AUTH_SOCK} -D > /dev/null 2>&1
+        exec /usr/bin/ssh-agent -a "${SSH_AUTH_SOCK}" -D > /dev/null 2>&1
         ;;
     *)
         exec $@;;
