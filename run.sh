9cda1a1c597774b4d910bc72041884d5def9af25
Will Maier <wcmaier@gmail.com>
2016-05-23 06:48:29 +0000

    Clear SSH_AUTH_SOCK from disk at start
    As reported in #5, the socket may be present when the agent is stopped
    and then started, in which case the agent refuses to restart.

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 case "$1" in
     ssh-agent)
+        rm -f "${SSH_AUTH_SOCK}"
         mkdir -p $(dirname "${SSH_AUTH_SOCK}")
         chmod 700 $(dirname "${SSH_AUTH_SOCK}")
         exec /usr/bin/ssh-agent -a ${SSH_AUTH_SOCK} -D > /dev/null 2>&1
